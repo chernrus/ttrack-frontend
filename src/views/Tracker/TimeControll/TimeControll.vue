dca49d2601e4cc3421ed8139c4c60eaf6c0a6ac3
Ruslan Chernov <chernruslan@gmail.com>
2020-08-30 16:26:04 +0300

    add responsive

--- src/views/Tracker/TimeControll/TimeControll.vue
+++ src/views/Tracker/TimeControll/TimeControll.vue
@@ -4,22 +4,22 @@
         <el-form :model="formModel"
             label-position="left">
             <el-row align="bottom" type="flex">
-                <el-col :span="6" >
+                <el-col :lg="6" :md="12" :xs="24" :sm="24">
                     <el-form-item label="Name">
                         <el-input v-model="formModel.name" placeholder="Task..."></el-input>
                     </el-form-item>
                 </el-col>
-                <el-col :span="3">
+                <el-col :lg="4" :md="6" :xs="12" :sm="12">
                     <el-form-item label="Start">
                         <el-time-picker class="w-100" v-model="formModel.start_t" placeholder="Start..."></el-time-picker>
                     </el-form-item>
                 </el-col>
-                <el-col :span="3">
+                <el-col :lg="4" :md="6" :xs="12" :sm="12">
                     <el-form-item label="End">
                         <el-time-picker class="w-100" v-model="formModel.end_t" placeholder="End..."></el-time-picker>
                     </el-form-item>
                 </el-col>
-                <el-col :span="6" >
+                <el-col :lg="6" :md="18" :xs="24" :sm="18">
                     <el-form-item label="Description">
                         <el-input v-model="formModel.description" placeholder="Description..." type="textarea" :autosize="{maxRows: 1}"></el-input>
                     </el-form-item>
